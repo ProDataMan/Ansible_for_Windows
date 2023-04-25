798446325197791006f09d9e0b7cf79806de59db
ProDataMan <Antoine.Victor@ProDataMan.com>
2023-04-25 03:20:51 -0700

    Update Ansible_Windows.ps1
    commented out commands

--- Ansible_Windows.ps1
+++ Ansible_Windows.ps1
@@ -1,24 +1,24 @@
-﻿Enable-PSRemoting -Force
+#Enable-PSRemoting -Force
 
-PowerShell -Version
+#PowerShell -Version
 
-(Get-Command New-PSSession).ParameterSets.Name
+#(Get-Command New-PSSession).ParameterSets.Name
 
-Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
+#Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
 
-Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+#Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-netstat -an | find "5985"
+#netstat -an | find "5985"
 
-Get-Service -Name winrm
+#Get-Service -Name winrm
 
-winrm get winrm/config
+#winrm get winrm/config
 
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+#winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 
 
 
-New-SelfSignedCertificate -DnsName "localhost" -CertStoreLocation "cert:\LocalMachine\My"
+#New-SelfSignedCertificate -DnsName "localhost" -CertStoreLocation "cert:\LocalMachine\My"
 
-New-Item -Path WSMan:\localhost\Listener -Transport HTTPS -Address * -CertificateThumbPrint "A3CEA98581B0F98120E0BA1413D9FE2AF0381FD3" -Force
+#New-Item -Path WSMan:\localhost\Listener -Transport HTTPS -Address * -CertificateThumbPrint "A3CEA98581B0F98120E0BA1413D9FE2AF0381FD3" -Force
 
