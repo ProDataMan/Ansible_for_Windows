2868fcbb26a924b3fe78ab27494591ae07862acb
ProDataMan <antoine.victor@prodataman.com>
2023-04-25 09:12:02 +0000

    updated script

--- Ansible_Windows.ps1
+++ Ansible_Windows.ps1
@@ -3,3 +3,22 @@
 PowerShell -Version
 
 (Get-Command New-PSSession).ParameterSets.Name
+
+Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
+
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+netstat -an | find "5985"
+
+Get-Service -Name winrm
+
+winrm get winrm/config
+
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+
+
+
+New-SelfSignedCertificate -DnsName "localhost" -CertStoreLocation "cert:\LocalMachine\My"
+
+New-Item -Path WSMan:\localhost\Listener -Transport HTTPS -Address * -CertificateThumbPrint "A3CEA98581B0F98120E0BA1413D9FE2AF0381FD3" -Force
+
