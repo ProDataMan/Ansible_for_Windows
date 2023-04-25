a890fbbabf5471a8847bc50a551adbb32f3f36d4
ProDataMan <Antoine.Victor@ProDataMan.com>
2023-04-25 03:22:50 -0700

    Update Ansible_Windows.ps1
    added firewall details

--- Ansible_Windows.ps1
+++ Ansible_Windows.ps1
@@ -1,24 +1,11 @@
-#Enable-PSRemoting -Force
-
-#PowerShell -Version
-
-#(Get-Command New-PSSession).ParameterSets.Name
-
-#Get-WindowsCapability -Online | ? Name -like 'OpenSSH*'
-
 #Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
 
-#netstat -an | find "5985"
-
-#Get-Service -Name winrm
-
-#winrm get winrm/config
-
 #winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-
-
 
 #New-SelfSignedCertificate -DnsName "localhost" -CertStoreLocation "cert:\LocalMachine\My"
 
 #New-Item -Path WSMan:\localhost\Listener -Transport HTTPS -Address * -CertificateThumbPrint "A3CEA98581B0F98120E0BA1413D9FE2AF0381FD3" -Force
 
+#Enable-PSRemoting -Force
+
+#If it still doesnt work create an inbound defender firewall rule for ports 5985,8986
